1ef9f098b41e8c772f11ebb73c244e209265c994
Toe Wai Aung <kotoeymb09@gmail.com>
2024-07-01 08:16:21 +0630

    first git commit

---
+++ StarSEexam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarSEexam
+//
+//  Created by Toe Wai Aung on 01/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
